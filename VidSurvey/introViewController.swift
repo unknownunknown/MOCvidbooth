69c0d5fcddc1ef2da81f40e997fb8b1cdd7dffe8
Tara Shi <unknownunknown@users.noreply.github.com>
2017-06-16 11:35:41 -0700

    Latest Working MOC video app
    3 interview questions version

---
+++ VidSurvey/introViewController.swift
@@ -0,0 +1,35 @@
+//
+//  introViewController.swift
+//  MOCVA
+//
+//  Created by Tara on 5/21/17.
+//  Copyright © 2017 Periard, Misty. All rights reserved.
+//
+
+import UIKit
+
+class introViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
